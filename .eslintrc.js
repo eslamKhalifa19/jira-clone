74664679131964b7093b63db9dbf93786bc4f82f
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-04-03 23:44:44 +0200

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  plugins: ["react", "import", "jsx-a11y"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+
+  parserOptions: {
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-is-valid": "warn",
+    "react/prop-types": "warn",
+    "react-hooks/rules-of-hooks": "warn",
+    "no-console": "warn",
+  },
+};
