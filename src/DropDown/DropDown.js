0bbaf9e23d3b661f4722767239effe0358e9bf66
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-04-27 18:29:36 +0200

    Reusable Dropdown

--- src/DropDown/DropDown.js
+++ src/DropDown/DropDown.js
@@ -1,92 +1,119 @@
-import React from "react";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState, useEffect, useRef } from "react";
+import { issueType, Priority } from "../DummyData";
 import "./dropdown.scss";
+import DropDownIcon from "./DropDownIcon";
+import DropDownImage from "./DropDownImage";
+import { useOnClickOutside } from "../hooks/useOnClickOutside";
 
-function ProjectSettings() {
+function DropDown({ options, label, selected, onSelectedChange }) {
+  const ref = useRef();
+
+  const [open, setOpen] = useState(false);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  };
+
+  useOnClickOutside(ref, () => setOpen(!open));
+
+  useEffect(() => {
+    const results = options.filter((result) => {
+      return result.label
+        .toString()
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase().trim());
+    });
+
+    setSearchResults(results);
+  }, [options, searchTerm]);
+
+  const handleClose = () => {
+    setSearchTerm("");
+  };
+
+  if (!searchResults) return options;
+
   return (
     <div>
-      <label htmlFor="category" className="dropdown__label">
-        Project Category
+      <label htmlFor="label" className="dropdown__label">
+        {label}
       </label>
       <div className="dropdown">
-        <div className="dropdown__bar">
-          <figure className="dropdown__shape">
-            <img
-              src="../img/profile-pic--1.jpg"
-              alt="Profile Picdropdown"
-              className="dropdown__img"
-            />
-          </figure>
-          <svg className="dropdown__icon dropdown__icon--orange">
-            <use xlinkHref="./img/sprite.svg#icon-arrow-up"></use>
-          </svg>
-          <svg className="dropdown__icon dropdown__icon--red">
-            <use xlinkHref="./img/sprite.svg#icon-error"></use>
-          </svg>
-          <svg className="dropdown__icon dropdown__icon--green">
-            <use xlinkHref="./img/sprite.svg#icon-bookmark"></use>
-          </svg>
-          <div className="dropdown__checkbox">
-            <svg className="dropdown__checkbox-icon">
-              <use xlinkHref="./img/sprite.svg#icon-check"></use>
-            </svg>
-          </div>
-          <span>Select</span>
+        <div
+          role="button"
+          tabIndex={0}
+          className="dropdown__bar"
+          onClick={() => setOpen(!open)}
+          onKeyPress={() => setOpen(!open)}
+        >
+          {selected === issueType || Priority ? (
+            <DropDownIcon icon={selected.icon} color={selected.color} />
+          ) : (
+            <DropDownImage src={selected.src} />
+          )}
+          <span>{selected.label}</span>
           <svg className="dropdown__icon-bar">
             <use xlinkHref="./img/sprite.svg#icon-chevron-down"></use>
           </svg>
         </div>
-        {/* Remove inline style to show the menu */}
-        <div style={{ display: "none" }} className="dropdown__menu">
-          <input type="text" className="dropdown__input" placeholder="Search" />
-          <svg className="dropdown__icon-close">
-            <use xlinkHref="./img/sprite.svg#icon-clear"></use>
-          </svg>
-          <div className="dropdown__select">
-            <div className="dropdown__option">
-              <figure className="dropdown__shape">
-                <img
-                  src="../img/profile-pic--2.jpg"
-                  alt="Profile Picdropdown"
-                  className="dropdown__img"
-                />
-              </figure>
-              <span>Marketing</span>
-            </div>
-            <div className="dropdown__option">
-              <svg className="dropdown__icon dropdown__icon--red">
-                <use xlinkHref="./img/sprite.svg#icon-arrow-up"></use>
-              </svg>
-              <span>Higest</span>
-            </div>
-            <div className="dropdown__option">
-              <svg className="dropdown__icon dropdown__icon--red">
-                <use xlinkHref="./img/sprite.svg#icon-arrow-up"></use>
-              </svg>
-              <span>High</span>
-            </div>
-            <div className="dropdown__option">
-              <svg className="dropdown__icon dropdown__icon--orange">
-                <use xlinkHref="./img/sprite.svg#icon-arrow-up"></use>
-              </svg>
-              <span>Medium</span>
-            </div>
-            <div className="dropdown__option">
-              <svg className="dropdown__icon dropdown__icon--green">
-                <use xlinkHref="./img/sprite.svg#icon-arrow-down"></use>
-              </svg>
-              <span>Low</span>
-            </div>
-            <div className="dropdown__option">
-              <svg className="dropdown__icon dropdown__icon--green">
-                <use xlinkHref="./img/sprite.svg#icon-arrow-down"></use>
-              </svg>
-              <span>Lowest</span>
+
+        {open && (
+          <div className="dropdown__menu " ref={ref}>
+            <input
+              type="text"
+              className="dropdown__input"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={handleChange}
+            />
+            <svg className="dropdown__icon-close" onClick={handleClose}>
+              <use xlinkHref="./img/sprite.svg#icon-clear"></use>
+            </svg>
+            <div
+              role="button"
+              tabIndex={0}
+              className="dropdown__select"
+              onClick={() => setOpen(!open)}
+            >
+              {searchResults.map((option) => {
+                if (option.value === selected.value) {
+                  return null;
+                }
+                return (
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    className="dropdown__option"
+                    key={option.value}
+                    onClick={() => {
+                      onSelectedChange(option);
+                      setOpen(!open);
+                    }}
+                    onKeyPress={() => {
+                      onSelectedChange(option);
+                      setOpen(!open);
+                    }}
+                  >
+                    {options == Priority || issueType ? (
+                      <DropDownIcon icon={option.icon} color={option.color} />
+                    ) : (
+                      <DropDownImage src={option.src} />
+                    )}
+                    {option.label}
+                  </div>
+                );
+              })}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
 }
 
-export default ProjectSettings;
+export default DropDown;
