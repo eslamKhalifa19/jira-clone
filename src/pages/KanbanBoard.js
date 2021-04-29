456f275fe55ef1726c7ebeb33339fd943c55f8d8
DarwishCodes <mrfunnym007@gmail.com>
2021-04-29 20:44:05 +0200

    KanbanBoard layout design

--- src/pages/KanbanBoard.js
+++ src/pages/KanbanBoard.js
@@ -2,8 +2,29 @@ import React from "react";
 
 function KanbanBoard() {
   return (
-    <div>
-      <h1>KanbanBoard</h1>
+    <div className="page-kanban-board">
+      <div className="board-row">
+        <div className="board-col">
+          <div className="u-padding-xs u-margin-bottom-xxs">
+            <p className="paragraph">BACKLOG 4</p>
+          </div>
+        </div>
+        <div className="board-col">
+          <div className="u-padding-xs">
+            <p className="paragraph">SELECTED FOR DEVELOPMENT 2</p>
+          </div>
+        </div>
+        <div className="board-col">
+          <div className="u-padding-xs">
+            <p className="paragraph">IN PROGRESS 1</p>
+          </div>
+        </div>
+        <div className="board-col">
+          <div className="u-padding-xs">
+            <p className="paragraph">DONE 1</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
