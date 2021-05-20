7be437188d5f7e2861dbcdfbdb125b0624fd54d9
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-05-20 08:54:52 +0200

    fix onClickOutsideModal redirect to Board

--- src/pages/CreateIssue.js
+++ src/pages/CreateIssue.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Redirect } from "react-router";
 import FormCreateIssue from "../FormCreateIssue/FormCreateIssue";
+import { useOnClickOutside } from "../hooks/useOnClickOutside";
 import Modal from "../Modal/Modal";
 function CreateIssue() {
+  const [showModal, setShowModal] = useState(true);
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => setShowModal(false));
+
   return (
-    <div className="page-form-create-issue">
-      <Modal>
-        <FormCreateIssue />
-      </Modal>
+    <div className="page-form-create-issue" ref={ref}>
+      {showModal ? (
+        <Modal>
+          <FormCreateIssue />
+        </Modal>
+      ) : (
+        <Redirect to="/" />
+      )}
     </div>
   );
 }
