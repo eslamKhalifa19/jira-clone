b17dfa0448f006cf3e2c58ddc4793318b04846ea
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-05-20 08:44:56 +0200

    make a reusable modal using portal and render createIssue inside it

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { Fragment, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 import BackDrop from "../BackDrop/BackDrop";
-import FormSettings from "../FormSettings/FormSettings";
 import "./modal.scss";
 
-function Modal() {
-  return (
-    <div>
+const modalRoot = document.getElementById("modal");
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+  return createPortal(
+    <Fragment>
       <BackDrop />
       <div className="modal">
         <div className="modal__wrap">
-          <div className="modal__container">
-            <FormSettings />
-          </div>
+          <div className="modal__container">{children}</div>
         </div>
       </div>
-    </div>
+    </Fragment>,
+    elRef.current
   );
-}
+};
 
 export default Modal;
