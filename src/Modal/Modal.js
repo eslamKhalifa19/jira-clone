101e2870f4932f94d271cdbc418228ae4d800f6d
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-05-23 22:55:48 +0200

    fix: clicking outside modal didnt work

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,11 +1,19 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { useOnClickOutside } from "../hooks/useOnClickOutside";
+
 import { createPortal } from "react-dom";
 import BackDrop from "../BackDrop/BackDrop";
 import "./modal.scss";
+import { Redirect } from "react-router";
 
 const modalRoot = document.getElementById("modal");
 
 const Modal = ({ children }) => {
+  const [showModal, setShowModal] = useState(true);
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => setShowModal());
+
   const elRef = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
@@ -17,13 +25,22 @@ const Modal = ({ children }) => {
   }, []);
   return createPortal(
     <Fragment>
-      <BackDrop />
-      <div className="modal">
-        <div className="modal__wrap">
-          <div className="modal__container">{children}</div>
-        </div>
-      </div>
+      {showModal ? (
+        <Fragment>
+          <BackDrop />
+          <div className="modal">
+            <div className="modal__wrap">
+              <div className="modal__container" ref={ref}>
+                {children}
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      ) : (
+        <Redirect to="/" />
+      )}
     </Fragment>,
+
     elRef.current
   );
 };
--- src/pages/CreateIssue.js
+++ src/pages/CreateIssue.js
@@ -1,23 +1,12 @@
-import React, { useRef, useState } from "react";
-import { Redirect } from "react-router";
+import React from "react";
 import FormCreateIssue from "../FormCreateIssue/FormCreateIssue";
-import { useOnClickOutside } from "../hooks/useOnClickOutside";
 import Modal from "../Modal/Modal";
 function CreateIssue() {
-  const [showModal, setShowModal] = useState(true);
-  const ref = useRef();
-
-  useOnClickOutside(ref, () => setShowModal(false));
-
   return (
-    <div className="page-form-create-issue" ref={ref}>
-      {showModal ? (
-        <Modal>
-          <FormCreateIssue />
-        </Modal>
-      ) : (
-        <Redirect to="/" />
-      )}
+    <div className="page-form-create-issue">
+      <Modal>
+        <FormCreateIssue />
+      </Modal>
     </div>
   );
 }
