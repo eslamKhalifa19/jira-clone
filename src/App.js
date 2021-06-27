ba42c8f93c84f6ef7f33a8ad84332cd231359ab5
Eslam Khalefa <eslamkhalefa19@gmail.com>
2021-06-28 00:46:29 +0200

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./sass/main.scss";
 import Header from "./Header/Header";
 import SideBar from "./SideBar/SideBar";
-import DrawerSearch from "./DrawerSearch/DrawerSearch";
-import "./sass/main.scss";
-import KanbanBoard from "../src/pages/KanbanBoard";
-import ProjectSettings from "../src/pages/ProjectSettings";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./pages/About";
-import CreateIssue from "./pages/CreateIssue";
-import PageError from "./PageError/PageError";
+import LoadingSpinner from "./LoadingSpinner/LoadingSpinner";
+const PageError = lazy(() => import("./PageError/PageError"));
+const About = lazy(() => import("./pages/About"));
+const ProjectSettings = lazy(() => import("../src/pages/ProjectSettings"));
+const CreateIssue = lazy(() => import("./pages/CreateIssue"));
+const DrawerSearch = lazy(() => import("./DrawerSearch/DrawerSearch"));
+const KanbanBoard = lazy(() => import("../src/pages/KanbanBoard"));
 function App() {
   return (
-    <Router>
-      <div>
-        <div className="container">
-          <Header />
-          <div className="content">
-            <SideBar />
-            <div className="page-view">
-              <Switch>
-                <Route exact path="/" component={KanbanBoard} />
-                <Route
-                  exact
-                  path="/project-settings"
-                  component={ProjectSettings}
-                />
-                <Route path="/about" component={About} />
-                <Route path="/create-issue" component={CreateIssue} />
-                <Route path="/search-issue" component={DrawerSearch} />
-                <Route render={() => <PageError />} />
-              </Switch>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Router>
+        <div>
+          <div className="container">
+            <Header />
+            <div className="content">
+              <SideBar />
+              <div className="page-view">
+                <Switch>
+                  <Route exact path="/" component={KanbanBoard} />
+                  <Route
+                    exact
+                    path="/project-settings"
+                    component={ProjectSettings}
+                  />
+                  <Route path="/about" component={About} />
+                  <Route path="/create-issue" component={CreateIssue} />
+                  <Route path="/search-issue" component={DrawerSearch} />
+                  <Route render={() => <PageError />} />
+                </Switch>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Suspense>
   );
 }
 
